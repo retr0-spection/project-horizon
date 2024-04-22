39e542e321ae4601b41768c41a410bdb16f6d4c3
retr0-spection <ratinailana@icloud.com>
2024-04-22 14:55:19 +0200

    /

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ dotenv.config();
 const app = express()
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  next()
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, DELETE, PATCH, OPTIONS",
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  if ("OPTIONS" == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 app.use(
